d6e3a624423f7cfe9e63160a7e6a44d87d17da54
amanmulani09 <mulaniaman0504@gmail.com>
2026-04-08 09:05:25 +0530

    fix: update pydantic_setting for BaseSettings

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from app.api.chat import router as chat_router
+from app.api.router import router as api_router
 
 app = FastAPI()
 
-app.include_router(chat_router,prefix="/api")
+app.include_router(api_router,prefix="/api")
